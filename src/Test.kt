c5c6555758b616aa0bc290dc4875363593af90c0
kum <gskebin4@gmail.com>
2018-11-28 18:32:54 +0900

    2번쨰

--- src/Test.kt
+++ src/Test.kt
@@ -4,8 +4,15 @@ val c: Pair<Char, Int> = Pair('A', 65)
 
 val aEqualsToB: Boolean = a == b
 val aEqualsToC: Boolean = a == c
-val aIdenticalToB : Boolean =  a === b
-val aIdenticalToC : Boolean =  a === c
+val aIdenticalToB: Boolean = a === b
+val aIdenticalToC: Boolean = a === c
+
+fun checkAge(age: Int) {
+
+    if (age < 0) {
+        throw IllegalAccessException("Invalid age : $age")
+    }
+}
 
 
 fun main(args: Array<String>) {
@@ -15,11 +22,27 @@ fun main(args: Array<String>) {
     println("aIdenticalToB : $aIdenticalToB")
     println("aIdenticalToC : $aIdenticalToC")
 
-    val carList : MutableList<Car>  = arrayListOf()
-    val sedanList : List<Sedan> = listOf()
+    val carList: MutableList<Car> = arrayListOf()
+    val sedanList: List<Sedan> = listOf()
 
-    FunctionClass.append(carList , carList)
+    FunctionClass.append(carList, carList)
 
+    val valid: Boolean = try {
+        true
+    } catch (e: Exception) {
+        false
+    }
+
+    val nullableString : String? = null
+
+    val nonNull : String = "F"
+
+    val str : String? = formatAddress("1" , "2")
+
+}
+
+fun formatAddress(line : String ,line2 : String) : String?{
+        return ""
 }
 
 
