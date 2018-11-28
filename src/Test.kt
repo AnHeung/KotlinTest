a2473b093b6876d427c276cab0a7442cae0adec5
kum <gskebin4@gmail.com>
2018-11-28 18:54:34 +0900

    3 번째

--- src/Test.kt
+++ src/Test.kt
@@ -1,3 +1,5 @@
+import java.awt.Image
+
 val a: Pair<Char, Int> = Pair('A', 65)
 val b = a
 val c: Pair<Char, Int> = Pair('A', 65)
@@ -27,11 +29,7 @@ fun main(args: Array<String>) {
 
     FunctionClass.append(carList, carList)
 
-    val valid: Boolean = try {
-        true
-    } catch (e: Exception) {
-        false
-    }
+    val valid: Boolean = try { true } catch (e: Exception) { false }
 
     val nullableString : String? = null
 
@@ -39,10 +37,36 @@ fun main(args: Array<String>) {
 
     val str : String? = formatAddress("1" , "2")
 
+    val postalCode : String = formatAddress("1","2") ?: "NONO"
+
+    val testVal : String = formatAddress("","")?:formatAddress2("")
+
+    val foo: String = "foo"
+
+    val bar : Int? = foo as? Int ?: 1
+
 }
 
 fun formatAddress(line : String ,line2 : String) : String?{
         return ""
+}
+
+fun formatAddress2(line : String) : String{
+    return ""
+}
+
+fun generateMap(address: String) :Image? {
+
+    val postal = formatAddress("2","3") ?: return null
+
+    return null
+}
+
+fun getString(): String?{
+
+    val post = formatAddress("", "") ?: return ""
+
+    return ""
 }
 
 
